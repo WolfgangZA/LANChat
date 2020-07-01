7ec20aeafe354641bed034093061ad7c529cb4d7
WolfgangZA <leon.barnard1996@gmail.com>
2020-07-01 09:34:12 +0200

    First Commit

---
+++ Backup/LANChat/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox
+        Me.Send = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(51, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(93, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(150, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 20)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(10, 38)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(262, 211)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Send
+        '
+        Me.Send.Location = New System.Drawing.Point(197, 255)
+        Me.Send.Name = "Send"
+        Me.Send.Size = New System.Drawing.Size(75, 23)
+        Me.Send.TabIndex = 4
+        Me.Send.Text = "Send"
+        Me.Send.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(10, 255)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(181, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(284, 308)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Send)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LANChat"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Send As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ LANChat/Form1.vb
@@ -0,0 +1,175 @@
+﻿Imports System.Threading
+Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class Form1
+    Private Const listenPort As Integer = 11000
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Your Name cannot be empty.")
+        ElseIf Button1.Text = "Connect" Then
+            IO.File.WriteAllText(Application.StartupPath + "\settings\name.txt", TextBox1.Text)
+            Button1.Text = "Disconnect"
+            TextBox1.Enabled = False
+            Send.Enabled = True
+
+            BackgroundWorker1.RunWorkerAsync()
+            Label1.Hide()
+            TextBox1.Hide()
+            Button1.Hide()
+            RichTextBox1.Show()
+            Send.Show()
+            TextBox2.Show()
+            TextBox2.Select()
+        ElseIf Button1.Text = "Disconnect" Then
+            Button1.Text = "Connect"
+
+            TextBox1.Enabled = True
+            Send.Enabled = False
+            BackgroundWorker1.CancelAsync()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim done As Boolean = False
+        Dim listener As New UdpClient(listenPort)
+        Dim groupEP As New IPEndPoint(IPAddress.Any, listenPort)
+        RichTextBox1.AppendText("Waiting for broadcast" & Environment.NewLine)
+
+        'sends connection message
+        Dim udpClient As New UdpClient()
+        udpClient.Connect("255.255.255.255", 11000)
+        Dim senddata As Byte()
+        senddata = Encoding.ASCII.GetBytes("!" + TextBox1.Text + " has connected to the network!")
+        udpClient.Send(senddata, senddata.Length)
+
+        'continues to listen for packets
+        Try
+
+            While Not done
+                'Handels Recieved packets
+                '! = Server Messsage
+                '* = Normal Chat Message
+                '@ = How many chat clients are there on the network? (Updates Label)
+
+                Dim bytes As Byte() = listener.Receive(groupEP)
+                'RichTextBox1.AppendText(groupEP.ToString())
+                RichTextBox1.AppendText(Environment.NewLine)
+
+                Dim msg As String = Encoding.ASCII.GetString(bytes, 0, bytes.Length)
+                Dim leftpart As String = Split(msg, ":")(0)
+                Dim rightpart As String = Split(msg, ":")(0)
+
+                Dim s As String = msg
+                Dim answer As Char
+                answer = s.Substring(0, 1)
+
+                If answer = "!" Then
+                    Dim Servermsg As String = Split(msg, "!")(1)
+                    'RichTextBox1.AppendText(Servermsg)
+                    RichTextBox1.SelectionColor = Color.Red
+                    RichTextBox1.SelectedText = Servermsg
+
+                ElseIf answer = "*" Then
+                    Dim chatmsg As String = Split(msg, "*")(1)
+                    RichTextBox1.AppendText(chatmsg)
+                ElseIf answer = "@" Then
+
+
+                Else
+                    RichTextBox1.SelectionColor = Color.Red
+                    RichTextBox1.SelectedText = "An unkown packet was recieved from: " + groupEP.ToString
+                End If
+
+               
+
+
+                'RichTextBox1.AppendText(msg)
+                'RichTextBox1.SelectionColor = Color.Red
+                'RichTextBox1.SelectedText = "Hello "
+                'RichTextBox1.SelectionColor = Color.Green
+                'RichTextBox1.SelectedText = "World"
+
+            End While
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            listener.Close()
+        End Try
+    End Sub
+
+
+    Private Sub Send_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Send.Click
+        If TextBox2.Text = "" Then
+            MsgBox("Message cannot be empty")
+        Else
+            Dim udpClient As New UdpClient()
+            udpClient.Connect("255.255.255.255", 11000)
+            Dim senddata As Byte()
+
+            senddata = Encoding.ASCII.GetBytes("*" + TextBox1.Text + ": " + TextBox2.Text)
+            udpClient.Send(senddata, senddata.Length)
+            TextBox2.Clear()
+        End If
+
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Dim errors As String = DateTime.Now
+        Dim correction As String = errors.Replace(" ", "@")
+        Dim coracao As String = correction.Replace("/", "-")
+        Dim final As String = coracao.Replace(":", "-")
+        'IO.File.Create(Application.StartupPath + "\logs\" + correction + ".txt")
+        If RichTextBox1.Visible = False Then
+
+        Else
+            System.IO.File.AppendAllText(Application.StartupPath + "\logs\" + final + ".rtf", RichTextBox1.Text)
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        If IO.File.Exists(Application.StartupPath + "\settings\name.txt") Then
+            TextBox1.Text = System.IO.File.ReadAllText(Application.StartupPath + "\settings\name.txt")
+        Else
+            IO.File.Create(Application.StartupPath + "\settings\name.txt")
+        End If
+
+
+        CheckForIllegalCrossThreadCalls = False
+        Send.Enabled = False
+        RichTextBox1.ReadOnly = True
+        RichTextBox1.BackColor = Color.White
+        RichTextBox1.Hide()
+        TextBox2.Hide()
+        Send.Hide()
+
+
+        'testing
+        Dim errors As String = DateTime.Now
+        Dim correction As String = errors.Replace(" ", "@")
+        errors.Replace(" ", "@")
+        'MsgBox(Application.StartupPath + "\logs\" + correction + ".txt")
+    End Sub
+
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        Me.AcceptButton = Send
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        Me.AcceptButton = Button1
+    End Sub
+
+End Class
